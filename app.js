833c5c7d357253dc78f6f8db89e3a66efb16c1a8
Rubens2214 <rubens_722@hotmail.es>
2022-08-18 19:44:45 -0300

    Arregle el formulario de registro

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ app.get("/", (req, res) =>{
     res.sendFile(path.join(__dirname, "./views/home.html"));
 });
 
-app.get("/register1", (req, res) =>{
-    res.sendFile(path.join(__dirname, "./views/register1.html"));
+app.get("/register", (req, res) =>{
+    res.sendFile(path.join(__dirname, "./views/register.html"));
 });
 app.get("/ofertas", (req, res) =>{
     res.sendFile(path.join(__dirname, "./views/ofertas.html"));
 });
-app.get("/register1", (req, res) =>{
+app.get("/register", (req, res) =>{
     res.send(Formulario);
 });
 app.post("/crear", (req, res)=>{
